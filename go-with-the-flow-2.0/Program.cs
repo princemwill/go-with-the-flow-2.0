5fedaee1d99e8c95e027140498786393d05757c3
Prince <princemwilliams@gmail.com>
2016-09-26 09:50:01 -0400

    Laid out structure for program. First lines of code.

--- go-with-the-flow-2.0/Program.cs
+++ go-with-the-flow-2.0/Program.cs
@@ -10,6 +10,19 @@ namespace go_with_the_flow_2._0
     {
         static void Main(string[] args)
         {
+
+            //check if lamp plugged in
+            Console.WriteLine("Is the lamp plugged in?");
+            Console.WriteLine();
+            Console.ReadKey();
+            //if no -> plug in 
+
+            //if yes, check bulb
+
+            //bulb burned? yes --> replace
+
+            //No, repair -->end
+
         }
     }
 }
